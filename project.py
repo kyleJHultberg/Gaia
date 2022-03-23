6818f528bb35f899a1256ebe92fef3ef49b8b8b9
Kyle <kylejhultberg@gmail.com>
2022-03-23 14:53:43 -0400

    Made createDefaultTemplate Scalable

--- project.py
+++ project.py
@@ -3,21 +3,6 @@ from utilities import FileController
 from body import Body
 
 
-"""
-Documentation:
-
-    Everything related to the overall project should start here.
-
-    self.name - The name of the project the user is creating.  For example,
-    if someone was creating a template for a website they were creating called
-    Facebook, self.name would be Facebook.
-
-    self.path_root = The idea is to allow the user to create a template that will
-    reside in the place of their choosing.  The end goal will be for a user to
-    choose the filepath from a GUI.
-
-"""
-
 
 class Project(Body):
     def __init__(self, name, path_root):
@@ -35,26 +20,37 @@ class Project(Body):
         else:
             os.mkdir(path)
             return path
+    
+    def createFile(self, path, file_name, mode, body):
+        with FileController(str(path), str(file_name), mode) as opened_file:
+            opened_file.write(body)
 
 
     def createDefaultTemplate(self):
         if os.path.isdir(self.full_folder_path) == False:
             self.createProjectFolder()
-
-        with FileController(str(self.full_folder_path), "main.py", "w") as opened_file:
-            opened_file.write(self.createMainDotPy())
-
+        
         self.www_folder_path = self.createFolder(self.full_folder_path + "www/")
-        with FileController(str(self.www_folder_path), "views.py", "w") as opened_file:
-            opened_file.write(self.createViewsDotPy())
-        with FileController(str(self.www_folder_path), "__init__.py", "w") as opened_file:
-            opened_file.write(self.createInitDotPy())
+        
+        self.createFile(self.full_folder_path, "main.py", "w", self.createMainDotPy())
+        self.createFile(self.www_folder_path, "views.py", "w", self.createViewsDotPy())
+        self.createFile(self.www_folder_path, "__init__.py", "w", self.createInitDotPy())
 
         self.static_folder_path = self.createFolder(self.full_folder_path + "www/static/")
 
         self.templates_folder_path = self.createFolder(self.full_folder_path + "www/templates/")
-        with FileController(str(self.templates_folder_path), "index.html", "w") as opened_file:
-            opened_file.write(self.createIndexDotHtml())
+
+        self.createFile(self.templates_folder_path, "index.html", "w", self.createIndexDotHtml())
+
+        # with FileController(str(self.full_folder_path), "main.py", "w") as opened_file:
+        #     opened_file.write(self.createMainDotPy())
+        # self.www_folder_path = self.createFolder(self.full_folder_path + "www/")
+        # with FileController(str(self.www_folder_path), "views.py", "w") as opened_file:
+        #     opened_file.write(self.createViewsDotPy())
+        # with FileController(str(self.www_folder_path), "__init__.py", "w") as opened_file:
+        #     opened_file.write(self.createInitDotPy())
+        # with FileController(str(self.templates_folder_path), "index.html", "w") as opened_file:
+        #     opened_file.write(self.createIndexDotHtml())
 
 
 
